298599227ec35fccbad9ecd8a77b05abcc905007
Arnob_Haque <arnobmonirulhaque@gmail.com>
2022-07-30 22:15:32 +0600

    Added Circlee

--- src/com/arnob/geometry/Circlee.java
+++ src/com/arnob/geometry/Circlee.java
@@ -1,2 +1,49 @@
-package com.arnob.geometry;public class Circlee {
+package com.arnob.geometry;
+
+public class Circlee {
+    public double radious =  1.0;
+    String color ="red";
+
+    public Circlee(){
+        this.radious = 1.0;
+        this.color = "red";
+    }
+
+    public Circlee(double radious) {
+        this.radious = radious;
+        this.color = "red";
+    }
+
+    public Circlee(double radious, String color) {
+        this.radious = radious;
+        this.color = color;
+    }
+
+    public double getRadious() {
+        return radious;
+    }
+
+    public void setRadious(double radious) {
+        this.radious = radious;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Circlee{" +
+                "radious=" + radious +
+                ", color='" + color + '\'' +
+                '}';
+    }
+    public double getarea() {
+         double area=(2*radious*(Math.PI));
+        return area;
+    }
 }
---
+++ src/com/arnob/geometry/Cylinder.java
@@ -0,0 +1,5 @@
+package com.arnob.geometry;
+
+public class Cylinder {
+
+}
